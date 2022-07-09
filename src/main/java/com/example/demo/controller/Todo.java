53135a7a673052a0dd0b0b4c37b395600bdb809b
cuonglk1102 <65998576+cuonglk1102@users.noreply.github.com>
2022-07-09 16:28:02 +0700

    todo list

---
+++ src/main/java/com/example/demo/controller/Todo.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+
+public class Todo {
+    public String title;
+    public String detail;
+    
+	public Todo() {
+
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+    
+    
+}
